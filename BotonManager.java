3917ac1a1fcd0d6e5e64d189305a9c7e5d2e7b44
aguperezpala <aguperezpala@e902fcd4-fc48-11dd-a8e2-f7cb3c35fcff>
2009-02-18 12:51:24 +0000

    ....

---
+++ BotonManager.java
@@ -0,0 +1,23 @@
+/* Clase que se va a encargar de manejar los botones:
+ * -Crearlos y posicionarlos.
+ * -Color (y cambiarlos de color a medida que pasa el tiempo.
+ * -Tiempo mostrado en pantalla.
+ * -Son apretados o no (activarlos y/o desactivarlos.
+ * 
+ */
+
+package hello;
+
+/**
+ *
+ * @author agustin
+ */
+public class BotonManager implements Runnable{
+
+    
+    public void run ()
+    {
+
+    }
+
+}
